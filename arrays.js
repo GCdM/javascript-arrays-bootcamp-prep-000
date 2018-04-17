b6160a207264f92ec8119f5b51da5398ccd346f3
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-04-17 19:22:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  
+  var newArray = [element, ...array]
+  return newArray
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  
+  return array.unshift(element)
 }
