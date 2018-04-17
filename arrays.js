fa0d9d4d118f08ae9a17a90aa958c4b4779cd704
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-04-17 19:51:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,20 +5,23 @@ function addElementToBeginningOfArray(array, element) {
   return newArray
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
 function addElementToEndOfArray(array, element) {
   var newArray = [...array, element]
   return newArray
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
+  array.push(element)
+  return array
 }
 function accessElementInArray(array, index) {
   return array[parseInt(index)]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift
+  array.shift
+  return array
 }
 function removeElementFromBeginningOfArray(array) {
   var newArray = array.slice(1)
