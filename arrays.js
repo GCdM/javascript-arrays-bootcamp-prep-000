46380884f0ec27e9cf2fd65a8892f78eed42d323
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-04-17 19:30:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,3 +20,7 @@ function accessElementInArray(array, index) {
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array.shift
 }
+function removeElementFromBeginningOfArray(array) {
+  var newArray = array.slice(1)
+  return newArray
+}
